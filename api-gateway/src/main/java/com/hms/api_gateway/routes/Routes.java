b4344aecc60614695d51539268b8c228dff79ee3
Vitor Mina <148635045+vitorhmina@users.noreply.github.com>
2024-11-13 14:25:44 +0000

    Circuit Breaker pattern implementation

--- api-gateway/src/main/java/com/hms/api_gateway/routes/Routes.java
+++ api-gateway/src/main/java/com/hms/api_gateway/routes/Routes.java
@@ -3,15 +3,20 @@ package com.hms.api_gateway.routes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
+import java.net.URI;
+
 import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
 
 @Configuration
 public class Routes {
@@ -34,6 +39,8 @@ public class Routes {
 
         return GatewayRouterFunctions.route("pet_service")
                 .route(RequestPredicates.path("/api/pet"), HandlerFunctions.http(petServiceUrl))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("petServiceCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")))
                 .build();
     }
 
@@ -43,6 +50,8 @@ public class Routes {
 
         return GatewayRouterFunctions.route("pet_service_swagger")
                 .route(RequestPredicates.path("/aggregate/pet-service/v3/api-docs"), HandlerFunctions.http(petServiceUrl))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("petServiceSwaggerCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")))
                 .filter(setPath("/api-docs"))
                 .build();
     }
@@ -53,6 +62,8 @@ public class Routes {
 
         return GatewayRouterFunctions.route("appointment_service")
                 .route(RequestPredicates.path("/api/appointment"), HandlerFunctions.http(appointmentServiceUrl))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("appointmentServiceCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")))
                 .build();
     }
 
@@ -62,6 +73,8 @@ public class Routes {
 
         return GatewayRouterFunctions.route("appointment_service_swagger")
                 .route(RequestPredicates.path("/aggregate/appointment-service/v3/api-docs"), HandlerFunctions.http(appointmentServiceUrl))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("appointmentServiceSwaggerCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")))
                 .filter(setPath("/api-docs"))
                 .build();
     }
@@ -72,6 +85,8 @@ public class Routes {
 
         return GatewayRouterFunctions.route("medical_record_service")
                 .route(RequestPredicates.path("/api/record"), HandlerFunctions.http(medicalRecordServiceUrl))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("medicalRecordServiceCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")))
                 .build();
     }
 
@@ -81,7 +96,17 @@ public class Routes {
 
         return GatewayRouterFunctions.route("medical_record_service_swagger")
                 .route(RequestPredicates.path("/aggregate/medical-record-service/v3/api-docs"), HandlerFunctions.http(medicalRecordServiceUrl))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("medicalRecordServiceSwaggerCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")))
                 .filter(setPath("/api-docs"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> fallbackRoute() {
+        return route("fallbackRoute")
+                .GET("/fallbackRoute", request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
+                        .body("Service Unavailable, please try again later"))
                 .build();
     }
 }
