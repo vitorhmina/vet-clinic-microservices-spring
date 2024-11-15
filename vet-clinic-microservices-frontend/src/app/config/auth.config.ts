d8df20071f67535bf87e3ddd9cb93bbeb2b4aea6
Vitor Mina <148635045+vitorhmina@users.noreply.github.com>
2024-11-15 22:25:49 +0000

    Angular Frontend initialization

---
+++ vet-clinic-microservices-frontend/src/app/config/auth.config.ts
@@ -0,0 +1,15 @@
+import { PassedInitialConfig } from 'angular-auth-oidc-client';
+
+export const authConfig: PassedInitialConfig = {
+  config: {
+    authority: 'http://localhost:8181/realms/vet-clinic-microservices-spring',
+    redirectUrl: window.location.origin,
+    postLogoutRedirectUri: window.location.origin,
+    clientId: 'angular-frontend-client',
+    scope: 'openid profile offline_access',
+    responseType: 'code',
+    silentRenew: true,
+    useRefreshToken: true,
+    renewTimeBeforeTokenExpiresInSeconds: 30,
+  }
+}
